5480076f74681398173b0dccb1662c231f150060
kewang <kewang.h.limbu@gmail.com>
2023-07-10 07:31:24 +0545

    types of properties- Readonly, WriteOnly,AutoImplemented

---
+++ OOPUsingC#/LearnProperties/Car.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnProperties
+{
+
+    /// <summary>
+    /// This is Example of Read Only Properties.
+    /// </summary>
+
+    public class Car
+    {
+        private int model = 2023;
+
+        private string manufacturer = "Tesla Pro Max";
+
+        public int Model_ID
+        {
+            get
+            {
+                return model;
+            }
+        }
+        public string Manufact_Name
+        {
+            get
+            {
+                return manufacturer;
+            }
+        }
+
+
+        ///<summary>
+        /// This is Example of Write only properties
+        /// </summary>
+
+
+        private int carModel = -1;
+        
+        private string owner = string.Empty;
+
+        public int _carModel
+        {
+            set { carModel = value; }
+        }
+
+        public string _owner
+        {
+            set { owner = value; }
+        }
+
+        public void DisplayData()
+        {
+            Console.WriteLine("====== Car =========");
+            Console.WriteLine("Model: {0}", carModel);
+            Console.WriteLine("Owner: {0}", owner);
+        }
+
+
+
+        ///<summary>
+        /// This is Example of Auto Implemented property
+        /// </summary>
+        
+
+        public int Price { get;set; }
+        public string CarName { get;set; }
+
+
+
+
+
+    }
+}
+
+
+
--- OOPUsingC#/LearnProperties/Program.cs
+++ OOPUsingC#/LearnProperties/Program.cs
@@ -11,7 +11,40 @@ namespace LearnProperties
             //Console.WriteLine("User Demad ike Color is " + bikeColor.MyBikeColor);
 
             //Now we will try to access the private variable using the property _MyBikeColor. Then it will work fine.
-            Console.WriteLine("User Demand Bike Color is " + bikeColor._MyBikeColor);
+           // Console.WriteLine("User Demand Bike Color is " + bikeColor._MyBikeColor);
+
+
+           ///<summary>
+           /// Read only proerties
+           ///</summary>
+
+          //Car evCar = new Car();
+          //Console.WriteLine("Model: {0}", evCar.Model_ID);
+          //Console.WriteLine("Model: {0}", evCar.Manufact_Name);
+
+
+          ///<summary>
+          /// Write only properties
+          /// </summary>
+          
+          //Car car2 = new Car();
+          //car2._carModel = 2023;
+          //car2._owner = "Kewang";
+          //car2.DisplayData();
+
+
+
+            ///<summary>
+            ///  Auto Implemented property
+            /// </summary>
+
+            Car car3 = new Car();
+            car3.Price = 5000000;
+            car3.CarName = "BMW";
+            Console.WriteLine("===  CAR ==========");
+            Console.WriteLine("Price: {0}", car3.Price);
+            Console.WriteLine( "Brand: {0}",car3.CarName);
+
 
         }
     }
