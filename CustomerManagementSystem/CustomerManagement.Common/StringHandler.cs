4aad2c02300104591111318861656aa8f685162a
kewang <kewang.h.limbu@gmail.com>
2024-01-07 17:31:54 +0545

    building reusable components

---
+++ CustomerManagementSystem/CustomerManagement.Common/StringHandler.cs
@@ -0,0 +1,27 @@
+﻿namespace CustomerManagement.Common
+{
+    public class StringHandler
+    {
+        // this method:  insert a space before each uppercase letter in a given string
+        public string InsertSpace(string source)
+        {
+
+            string result= string.Empty;
+
+            if(!string.IsNullOrWhiteSpace(source)) 
+            {
+            
+                foreach(char letter in source)
+                {
+                  if(char.IsUpper(letter))
+                    {
+                        result += " ";
+                    }
+                    result += letter;
+                }
+            }
+
+            return result;
+        }
+    }
+}
