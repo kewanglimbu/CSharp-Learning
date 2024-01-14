1ab895894dfb9e059d090b3d1785eb706d65dc06
kewang <kewang.h.limbu@gmail.com>
2024-01-14 16:13:18 +0545

    Interface-based polymorphism: LoggingService

---
+++ CustomerManagementSystem/CustomerManagement.Common/LoggingService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerManagement.Common
+{
+    public static class LoggingService
+    {
+        public static void WriteToFile(List<ILoggable> itemToLog)
+        {
+            foreach(var item in itemToLog)
+            {
+                Console.WriteLine(item.Log());
+            }
+        }
+    }
+}
